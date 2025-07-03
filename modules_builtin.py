94342c6f0a1733d89cb47f947576e25643d41ca7
cyber-emp <chris.sebastiao@gmail.com>
2025-07-03 08:54:23 +0200

    Create modules_builtin.py

---
+++ modules_builtin.py
@@ -0,0 +1,22 @@
+#How to use built-in modules in Python
+# Built-in modules are part of the Python standard library and can be imported directly without needing to install them.
+# They provide a wide range of functionalities, such as mathematical operations, file handling, and more
+
+# Example of using a built-in module: math
+
+# the python standard library is a vast collection of built-in modules.
+
+import math
+import random
+import datetime
+import os
+import sys
+
+
+
+
+print(random.randint(0, 100))
+print(math.pi)
+print(datetime.datetime.now())
+print(os.getcwd())  # Get the current working directory
+print(sys.version)  # Get the Python version  
